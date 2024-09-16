5becc9d78e3ca087309fcb7180e3994657b1c51f
marco-duart <marcoaurelio_db@hotmail.com>
2024-09-16 11:13:53 -0300

    changed default background style: color

--- src/assets/styles/global-styles.ts
+++ src/assets/styles/global-styles.ts
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: ${({ theme }) => theme.fonts.body};
-    background-color: ${({ theme }) => theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.backgroundSecondary};
     color: ${({ theme }) => theme.colors.text};
   }
 `;
